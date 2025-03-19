841bb1ad66fa05a72f6d90f86230bcbfaf1d564c
Jason RG <jasonrodriguezempleo@gmail.com>
2025-03-19 20:44:31 +0100

    Añadiendo carrito y css

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import {Route, Routes } from "react-router-dom";
+import Carrito from './components/Carrito';
 
 
 function App() {
   return (
     <>
-          <Navbar />
+      {/* <Navbar /> */}
 
-<Routes>
-  <Route path="/" element={<Home />}></Route>
-  <Route path="/carrito" element={<Carrito />}></Route>
-  <Route path="/productos" element={<Productos />}></Route>
-  {/* ruta dinámica añadiendo un parámetro
-  /coches/100
-  /coches/2
-  /coches/ford
-  */}
-  {/* <Route path="/coche/:id" element={<Coche />}></Route>
-  <Route path="/usuarios" element={<Usuarios />}></Route>
-  <Route path="/usuario/:id" element={<Usuario />}></Route> */}
+      {/* <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/carrito" element={<Carrito />}></Route>
+        <Route path="/productos" element={<Productos />}></Route> */}
+        {/* ruta dinámica añadiendo un parámetro
+        /coches/100
+        /coches/2
+        /coches/ford
+        */}
+        {/* <Route path="/coche/:id" element={<Coche />}></Route>
+        <Route path="/usuarios" element={<Usuarios />}></Route>
+        <Route path="/usuario/:id" element={<Usuario />}></Route> */}
 
-</Routes>
+      {/* </Routes> */}
 
+      <Carrito />
     </>
 
   );
